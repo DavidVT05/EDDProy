06e7081c51482f1b9de45dfc8337b0dc6b045c8c
DAVIDVT05 <al23760344@ite.edu.mx>
2024-11-30 10:52:57 -0800

    ultimo cambio de listas dobles

--- EDDProy/Estructuras Lineales/Clases/ListasDobles.cs
+++ EDDProy/Estructuras Lineales/Clases/ListasDobles.cs
@@ -65,7 +65,77 @@ namespace EDDemo.Estructuras_Lineales.Clases
         }
         public int Eliminar(int posicion)
         {
-
+            if(Cabeza == null && Fin == null) // comprobamos si la lista se encuentra vacia
+            {
+                MessageBox.Show("La lista se encuentra vacia.");
+                return -1; // Se indica que no se pudo eliminar ningun nodo
+            }
+            if(posicion < 1) // Se realiza una revision para saber si la posicion solicitada es valida.
+            {
+                MessageBox.Show("La poscion ingresada no es valida.");
+                return -1;
+            }
+            int pos = 1;
+            NodosActuales aux = Cabeza;
+            while (pos < posicion && aux != null) // Se realiza un recorido para encontrar la posicion indicada
+            {
+                aux = aux.sig;
+                pos++;
+            }
+            if(aux != null) // Cuando se encuentra el nodo en la posicion inidicada se realiza lo siguiente
+            {
+                if(aux == Cabeza) // cuando el nodo a eliminar es el primero
+                {
+                    Cabeza = aux.sig; // Se recorre el inicio al siguiente nodo
+                    if(Cabeza != null) //Si la lista nos queda vacia
+                    {
+                        Cabeza.ant = null; //se actualiza el puntero 
+                    }
+                }
+                else if(aux == Fin) // cuando el nodo al eliminar se encuentra al final
+                {
+                    Fin = aux.ant; // Se regresa un nodo para asignar que es el nuevo fin
+                }
+                else // cuando el nodo se encuentra en medio de la lista
+                {
+                    aux.ant.sig = aux.sig; // apuntamos el nodo anterior al nodo sigueinte
+                    aux.sig.ant = aux.ant; // apuntamos el nodo siguiente al nodo anterior
+                }
+                int datoEliminado = aux.Valor; // Se guarda el dato del nodo que se quiere eliminar
+                // se limpia el nodo 
+                aux.sig = null;
+                aux.ant = null;
+                aux = null;
+                return datoEliminado; //Se regresa el dato del nodo indicado a eliminar
+            }
+            else
+            {
+                MessageBox.Show("El nodo buscado no se encontro.");
+                return 0; // si no se encuentra el nodo en la posicion inidicada
+            }
+        }
+        public NodosActuales Buscar(int dato)
+        {
+            NodosActuales aux = Cabeza;
+            while(aux != null)
+            {
+                if(aux.Valor == dato)
+                {
+                    return aux;
+                }
+                aux = aux.sig;
+            }
+            return null;
+        }
+        public void Mostrar(ListBox listBox)
+        {
+            NodosActuales aux = Cabeza;
+            listBox.Items.Clear(); // limpiamos el listbox antes de mostrar los valores
+            while (aux != null)
+            {
+                listBox.Items.Add(aux.Valor);
+                aux = aux.sig;
+            }
         }
     }
 }
--- EDDProy/Estructuras Lineales/frmListasDobles.cs
+++ EDDProy/Estructuras Lineales/frmListasDobles.cs
@@ -22,22 +22,40 @@ namespace EDDemo.Estructuras_Lineales
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-
+            int dato = int.Parse(txtValor.Text);
+            NodosActuales encontrado = dobles.Buscar(dato);
+            if(encontrado != null)
+            {
+                MessageBox.Show("El Nodo si se encuentra en la lista");
+            }
+            else
+            {
+                MessageBox.Show("El nodo no se encontro en la lista.");
+            }
         }
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-
+            int posicion = int.Parse(txtPosicion.Text); // Se obitenen la posicion ingresada
+            int dato = int.Parse(txtValor.Text); // Se obtiene el valor ingresado
+            dobles.Insertar(posicion, dato);
+            MessageBox.Show("El nodo se ingreso correctamente.");
         }
 
         private void btmMostrar_Click(object sender, EventArgs e)
         {
-
+            dobles.Mostrar(listDobles);
         }
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
-
+            int posicion = int.Parse(txtPosicion.Text);
+            int datoEliminado = dobles.Eliminar(posicion);
+            if(datoEliminado != -1)
+            {
+                MessageBox.Show("El nodo ha sido eliminado");
+            }
+            dobles.Mostrar(listDobles);
         }
     }
 }
